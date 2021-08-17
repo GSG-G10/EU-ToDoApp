4a9f821f2dfdf38a207eac8b4ebfde77987891a0
umar <umars.salah@gmail.com>
2021-08-17 16:30:03 +0300

    use compression middleware Relates #5

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { join } = require('path');
+const compression = require('compression');
 
 const app = express();
 app.set('PORT', process.env.PORT || 7070);
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
