ad8be3a5b031f4d90d9984e8efe55284685106a3
umar <umars.salah@gmail.com>
2021-08-17 18:35:23 +0300

    Handle errors pages Relates #18

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { join } = require('path');
 const compression = require('compression');
 
+const routes = require('./routes');
+
 const app = express();
 app.set('PORT', process.env.PORT || 7070);
 app.use(compression());
@@ -9,5 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(join(__dirname, '..', 'public')));
+app.use(routes);
 
 module.exports = app;
