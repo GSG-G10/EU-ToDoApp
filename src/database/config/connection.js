9de092969a12ed7fa61f4faeeaf19364eb2c03d8
umar <umars.salah@gmail.com>
2021-08-17 17:11:30 +0300

    Edit DB_URL Relates #4

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 let DB_URL = '';
 switch (process.env.NODE_ENV) {
-  case process.env.NODE_ENV === 'test':
+  case 'test':
     DB_URL = process.env.TEST_DB_URL;
     break;
-  case process.env.NODE_ENV === 'production':
+  case 'production':
     DB_URL = process.env.DB_URL;
     break;
   default: throw new Error('No Database URL!');
