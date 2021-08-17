080c3d8c3381a21aa2fac033e0d8620ab15f3d78
umar <umars.salah@gmail.com>
2021-08-17 17:20:42 +0300

    Edit tasks table Relates #4

--- src/database/config/data.sql
+++ src/database/config/data.sql
@@ -12,7 +12,10 @@ CREATE TABLE users (
 CREATE TABLE tasks  (
   id           SERIAL       PRIMARY KEY ,
   user_id    INTEGER      REFERENCES users(id),
+  title VARCHAR(255) NOT NULL,
   task_description VARCHAR(255) NOT NULL,
+  due_time DATETIME NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT FALSE,
 );
 
 COMMIT;
