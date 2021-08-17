d12b38e39c4209b859be50fe33699cbcc0164166
ElhamFadel <elham2000Fadel@gmail.com>
2021-08-17 19:16:26 +0300

    fix conflict

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+const { clientError, serverError } = require('./error');
 module.exports = {
     getTask : require('./getTask'),
+    clientError : clientError,
+    serverError : serverError
 }
-=======
-const { clientError, serverError } = require('./error');
-
-module.exports = { clientError, serverError };
->>>>>>> 4de2d17bff3a49a1055d346d74415079f8fa861f
